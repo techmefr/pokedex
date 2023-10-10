c6cb0d1c473b30ebccdf6dd04e3333cd7ecbfe93
techmefr <compigni.gaetan@gmail.com>
2023-10-10 15:46:44 +0200

    create a variable about pokemonList

--- src/component/PokemonCard.jsx
+++ src/component/PokemonCard.jsx
@@ -1,25 +1,26 @@
 
 const pokemonList = [
     {
-    name: "bulbasaur",
-    imgSrc:
+name: "bulbasaur",
+imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
-    name: "Bulbasaur",
+name: "mew",
     },
 ];
 
 function PokemonCard(){
+    const pokemon = pokemonList[0]
+
     return (   
         <div>
-<figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="BulbaSaur"/>
-</figure>
-<figcaption>Bulbasaur</figcaption>
-    
-    </div>
-)
+            <figure>
+                <img src={pokemon.imgSrc} alt={pokemon.name} />
+            </figure>
+            <figcaption>{pokemon.name}</figcaption>
+        </div>
+    )
 }
 
 export default PokemonCard;
