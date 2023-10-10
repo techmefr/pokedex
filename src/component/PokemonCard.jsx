4b94d4398c6fcb521aad4c231740e420b95d36c1
techmefr <compigni.gaetan@gmail.com>
2023-10-10 16:58:17 +0200

    create a second pokemon and works if you don't have a picture

--- src/component/PokemonCard.jsx
+++ src/component/PokemonCard.jsx
@@ -10,17 +10,48 @@ name: "mew",
     },
 ];
 
-function PokemonCard(){
-    const pokemon = pokemonList[0]
-
-    return (   
+function PokemonCard() {
+    const pokemon = pokemonList[1];
+    let altText = pokemon.name; // Par défaut, utilisez le nom du Pokémon
+    if (pokemon.imgSrc === undefined || pokemon.imgSrc === null) {
+        return (
+            <div>
+                <figure>
+                    {pokemon.imgSrc ? (
+                        <img src={pokemon.imgSrc} alt={pokemon.name} />
+                    ) : ("???")}
+                </figure>
+                <figcaption>
+                    {pokemon.name}
+                </figcaption>
+            </div>
+        );
+        }
+    else {
+    return (
         <div>
             <figure>
-                <img src={pokemon.imgSrc} alt={pokemon.name} />
+                <img src={pokemon.imgSrc} alt={altText} />
             </figure>
             <figcaption>{pokemon.name}</figcaption>
         </div>
-    )
+    );
+}
 }
 
+//function PokemonCard() {
+//     const pokemon = pokemonList[1];
+    // return ( 
+//       <div>
+//         <figure>
+//           <img
+//             src={pokemon.imgSrc || "???"} // Utilisation de l'opérateur ternaire ici
+//             alt={pokemon.imgSrc ? pokemon.name : "???"} // Et ici
+//           />
+//         </figure>
+//         <figcaption>{pokemon.name}</figcaption>
+    //   </div>
+ //     );
+//   }
 export default PokemonCard;
+
