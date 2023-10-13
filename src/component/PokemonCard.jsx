f5f85eaa6a80d449107be11e963a50ebed9457bb
techmefr <compigni.gaetan@gmail.com>
2023-10-13 09:54:53 +0200

    correction pokemonCard update

--- src/component/PokemonCard.jsx
+++ src/component/PokemonCard.jsx
@@ -1,25 +1,57 @@
 
 const pokemonList = [
     {
-    name: "bulbasaur",
-    imgSrc:
+name: "bulbasaur",
+imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
-    name: "Bulbasaur",
+name: "mew",
     },
 ];
 
-function PokemonCard(){
-    return (   
+function PokemonCard() {
+    const pokemon = pokemonList[1];
+    let altText = pokemon.name; // Par défaut, utilisez le nom du Pokémon
+    if (pokemon.imgSrc === undefined || pokemon.imgSrc === null) {
+        return (
+            <div>
+                <figure>
+                    {pokemon.imgSrc ? (
+                        <img src={pokemon.imgSrc} alt={pokemon.name} />
+                    ) : ("???")}
+                </figure>
+                <figcaption>
+                    {pokemon.name}
+                </figcaption>
+            </div>
+        );
+        }
+    else {
+    return (
         <div>
-<figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="BulbaSaur"/>
-</figure>
-<figcaption>Bulbasaur</figcaption>
-    
-    </div>
-)
+            <figure>
+                <img src={pokemon.imgSrc} alt={altText} />
+            </figure>
+            <figcaption>{pokemon.name}</figcaption>
+        </div>
+    );
+}
 }
 
+//function PokemonCard() {
+//     const pokemon = pokemonList[1];
+    // return ( 
+//       <div>
+//         <figure>
+//           <img
+//             src={pokemon.imgSrc || "???"} // Utilisation de l'opérateur ternaire ici
+//             alt={pokemon.imgSrc ? pokemon.name : "???"} // Et ici
+//           />
+//         </figure>
+//         <figcaption>{pokemon.name}</figcaption>
+    //   </div>
+ //     );
+//   }
 export default PokemonCard;
+
