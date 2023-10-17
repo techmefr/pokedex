a787fe2a2b662de6446c129b3fcd6d177e594167
techmefr <compigni.gaetan@gmail.com>
2023-10-17 09:40:04 +0200

    add proptypes and recreate

--- src/component/PokemonCard.jsx
+++ src/component/PokemonCard.jsx
@@ -1,33 +1,37 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
 function PokemonCard(props) {
-    const { pokemon } = props;
+  const { pokemon } = props;
 
-    if (!pokemon || !pokemon.name) {
+  if (!pokemon || !pokemon.name) {
     return (
-        <div>
-            <figure>
-                <img src="???" alt="???" />
-            </figure>
-            <figcaption>???</figcaption>
-        </div>
-    );
-    }
-
-    const { name, imgSrc } = pokemon;
-
-    return (
-    <div>
+      <div>
         <figure>
-        {imgSrc ? (
-            <img src={imgSrc} alt={name} />
-        ) : (
-        <img src="???" alt="???" />
-        )}
+          <img src="???" alt="???" />
         </figure>
-        <figcaption>{name}</figcaption>
-    </div>
+        <figcaption>???</figcaption>
+      </div>
     );
+  }
+
+  const { name, imgSrc } = pokemon;
+
+  return (
+    <div>
+      <figure>
+        {imgSrc ? <img src={imgSrc} alt={name} /> : <img src="???" alt="???" />}
+      </figure>
+      <figcaption>{name}</figcaption>
+    </div>
+  );
 }
+
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string,
+    imgSrc: PropTypes.string,
+  }),
+};
 
 export default PokemonCard;
